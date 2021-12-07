e0c80f03a513174f6f96b115b7147a4042e7a1d8
Lissu <lissu@kakao.com>
2021-12-07 19:30:20 +0900

    set work experience

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -4,6 +4,16 @@ const WorkExperience = () => {
   return (
     <div className='work-experience-wrapper'>
       <div className='main-title'>Work Experience</div>
+      <div className='workbox-section'>
+        <div className='my-position'>Middle-Tier Web Developer</div>
+        <div>비즈아이솔루션</div>
+        <div>2017.02 ~ 2019.08</div>
+      </div>
+      <div className='workbox-section'>
+        <div className='my-position'>Front-End Developer</div>
+        <div>넥스트레벨스튜디오</div>
+        <div>2019.11 ~ </div>
+      </div>
     </div>
   )
 }
